4ebd93610960caa4fd19ca0e3b7999c5ba17b0ef
Miguel Jordán <alejordan81097@gmail.com>
2020-02-11 01:21:08 -0400

    refactor tests, repeated lines

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -3,23 +3,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class FizzBuzzTest {
+	FizzBuzz fb = new FizzBuzz();
 	
 	@Test
 	public void testWithOne() {
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("1", fb.generarUno(1));
 	}
 
 	@Test
 	public void testWithThree() {
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("Fizz", fb.generarUno(3));
 	}
 	
 	@Test
 	public void testWithFive() {
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("Buzz", fb.generarUno(5));
 	}
+	
+	@Test
+	public void testWithFifteen() {
+		assertEquals("FizzBuzz", fb.generarUno(15));
+	}
+	
+	
 
 }
